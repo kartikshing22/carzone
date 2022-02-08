8f7128ed49f8a7fca6304fc53df8d410b5e54a9c
kartikshing22 <kartikshing22@gmail.com>
2022-02-08 12:58:59 +0530

    added features into admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
+import imp
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;"/>'.format(object.photo.url))
+    thumbnail.short_description="photo"
+    list_display=('id','thumbnail','first_name','designation','created_date') #this is for make show clickable in admin panel
+    list_display_links=('first_name','thumbnail','id') #this is for make fielsd clickable in admin panel
+    search_fields=('first_name','last_name','designation') #this is search field,django gives
+    list_filter=('designation',) #this is filter area by django
+admin.site.register(Team,TeamAdmin)
